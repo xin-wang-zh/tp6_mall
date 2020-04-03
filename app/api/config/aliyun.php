de170060f56f062697ae7bebd5379440f01e6c84
xin-wang-zh <134514586@qq.com>
2020-04-03 16:40:46 +0800

    delete aliyunSecret

--- app/api/config/aliyun.php
+++ app/api/config/aliyun.php
@@ -9,7 +9,7 @@
 return [
     "host" => "dysmsapi.aliyuncs.com",
     "accessKeyId" => 'LTAI005fVWKG2Qxr',
-    "accessSecret" => "2kV1V4H1WGn6iEetOW8BAx59oqwA1z",
+    "accessSecret" => "",
     "regionId" =>"cn-hangzhou",
     "SignName"=>"为未来而努力",
     "TemplateCode" => "SMS_158946910",
